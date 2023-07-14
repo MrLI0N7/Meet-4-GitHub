0fe53f2953180cff6e1585a27b22ee803f0c4c68
M Fathurahman Aldesrand <fathuraldesrand03@gmail.com>
2023-07-14 14:38:46 +0700

    nama dan nim M Fathurahman A

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Liyan Made Leon WAYAN - (1302213037)")
 	fmt.Println("M. Daffa Raygama - (1301223295)")
+	fmt.Println("Mochamad Fathurahman Aldesrand - (1301220325)")
 }
