d5d3e5a5e66f3aa7896c1e5bffa62a8aaab547d2
mfchrzaini <fachruzaini@gmail.com>
2023-07-14 14:56:07 +0700

    nama dan nim Fachru Zaini

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Liyan Made Leon WAYAN - (1302213037)")
 	fmt.Println("M. Daffa Raygama - (1301223295)")
 	fmt.Println("Mochamad Fathurahman Aldesrand - (1301220325)")
+	fmt.Println("Mochamad Fachru Zaini Cahyadi - (6701223093)")
 }
